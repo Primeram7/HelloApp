f0792cdb839c1d3900567a6d864a353e3559a999
Primeram7 <iit70739@gmail.com>
2026-04-05 18:23:31 +0530

    Add files via upload

---
+++ HelloAppUC6.java
@@ -0,0 +1,14 @@
+public class HelloAppUC6 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            StringBuilder sb = new StringBuilder();
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+            String result = sb.substring(0, sb.length() - 2);
+            System.out.println("Hello, " + result + "!");
+        }
+    }
+}
